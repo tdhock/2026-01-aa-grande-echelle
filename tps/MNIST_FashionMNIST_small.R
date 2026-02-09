b237f45caaab08e992b24106a8ea5b02c563e595
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-02-09 17:20:20 -0500

    frfigs

--- tps/MNIST_FashionMNIST_small.R
+++ tps/MNIST_FashionMNIST_small.R
@@ -3,8 +3,11 @@ if(!file.exists("MNIST_FashionMNIST.csv"))download.file("https://rcdata.nau.edu/
 library(data.table)
 mfash <- fread("MNIST_FashionMNIST.csv")
 names(mfash)[1:9]
+mfash[, .(data.name, y, `0`, `1`)]
+mfash[, table(y, data.name)]
 set.seed(1)
 smallfash <- mfash[, .SD[sample(700)], by=.(data.name, y)]
+smallfash[, .(data.name, y, `0`, `1`)]
 smallfash[, table(y, data.name)]
 fwrite(smallfash, "MNIST_FashionMNIST_small.csv")
 system("du -ms MNIST_FashionMNIST.csv MNIST_FashionMNIST_small.csv")
