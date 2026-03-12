becee08f998962f685cc02a9a755ccde01cd8da1
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-03-11 21:55:02 -0400

    err

--- demos/pybind11-numpy/add_module-run.py
+++ demos/pybind11-numpy/add_module-run.py
@@ -1,1 +1,10 @@
 import add_module
+add_module.add_arrays(1, 2)
+import numpy as np
+x=np.arange(2)
+y=np.array([3,4])
+add_module.add_arrays(x,y)
+m=np.random.randn(1,2)
+add_module.add_arrays(x,m)
+z=np.array([1])
+add_module.add_arrays(x,z)
